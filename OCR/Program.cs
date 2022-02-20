2296f4b6041768c4c69c75053cf2a1913ec01371
Ajay Kumar <ajaykumaraj365@gmail.com>
2022-02-20 22:59:50 +0530

    Updating the latest code

---
+++ OCR/Program.cs
@@ -0,0 +1,47 @@
+﻿using IronOcr;
+
+namespace OCR
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var directoryPath = args[0];
+            foreach (var file in Directory.GetFiles(directoryPath))
+            {
+                ExtractFile(file);
+            }
+        }
+
+        internal static void ExtractFile(string filePath)
+        {
+            try
+            {
+                Console.WriteLine("Processing: " + Path.GetFullPath(filePath));
+                var directory = Path.GetDirectoryName(filePath);
+                var filename = Path.GetFileNameWithoutExtension(filePath);
+                var outputFilePath = Path.Combine(directory ?? "", filename + ".txt");
+                var Ocr = new IronTesseract();
+                using (var Input = new OcrInput(filePath))
+                {
+                    var Result = Ocr.Read(Input);
+                    Result.SaveAsTextFile(outputFilePath);
+                }
+                if (File.Exists(outputFilePath))
+                {
+                    Console.BackgroundColor = ConsoleColor.Green;
+                    Console.WriteLine("SUCCESS.");
+                    Console.ResetColor();
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                var message = string.Format("ERROR at {0}:-\nMessage: {1}\nStackTrace: {2}", Path.GetFullPath(filePath), ex.Message, ex.StackTrace);
+                Console.WriteLine(message);
+                Console.ResetColor();
+            }
+        }
+    }
+}
+
